392928b26602d77e38a67e4f904b1b1fb983def9
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-21 16:02:58 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,15 @@ ActiveRecord::Schema.define(version: 20131021131448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "quantity"
+    t.decimal  "price",       precision: 9, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
