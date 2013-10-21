b47573b108e7c36363d7fcfc482836b031e86294
DanL <ddlinde@hotmail.com>
2013-10-21 14:33:10 +0100

    create new products, individual product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,18 @@
 class ProductsController < ApplicationController
-
 	def index
 		@products = Product.all
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+	
+	def create
+		product = Product.create(params[:product].permit(:name, :description, :quantity, :price))
+		redirect_to product_path(product)
 	end
 end
--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -1,9 +1,32 @@
 require 'spec_helper'
 
-describe 'products page' do |variable|
+describe 'products page' do
 	it 'should display products' do
 		Product.create(name: 'a product')
 		visit '/products'
 		expect(page).to have_content 'a product'
+	end
+end
+
+describe 'an individual product' do
+	before(:each) do
+		Product.create(name: 'a product')
+	end
+	it 'should have its own page' do
+		visit '/products'
+		click_link "a product"
+		expect(page).to have_content 'a product'
+	end
+end
+
+describe 'a new product form' do
+	it 'create a new product' do
+		visit '/products/new'
+		fill_in 'name', with: 'Brand new product'
+		fill_in 'description', with: 'description of product'
+		fill_in 'quantity', with: 10
+		fill_in "price", with: 9.99
+		click_button "Create Product"
+		expect(page).to have_content 'Brand new product'
 	end
 end
