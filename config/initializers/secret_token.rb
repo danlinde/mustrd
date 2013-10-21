c4df7d9e7223652eadd2c3ee46d5b131503a89e1
DanL <ddlinde@hotmail.com>
2013-10-21 13:28:52 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mustrd::Application.config.secret_key_base = '656303a70f7fb3b2bbb314ad4573ea0f78598cf088770d29efabd48ecb49f9f850768b86e2ba0f812761d1378d3984190f55b186d3340edfe58a4f2fca582817'
