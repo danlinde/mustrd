358603fff239a87a30ad1180d0de7e758919f4b6
Harrisam <samantha.s.harris@googlemail.com>
2013-10-22 17:31:27 +0100

    Add seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+products = Product.create([{ name: 'iPhone', description: 'Not just a phone.', quantity: 10, price: 599.90 },
+						   { name: 'cuddly bear', description: 'soft and sweet', quantity: 5, price: 25.00 },
+						   { name: 'Goldfish', description: 'orange and wet (preferably)', quantity: 15, price: 30.00 },
+						   { name: 'Air rifle', description: 'tiny and does not work', quantity: 4, price: 8.00 },
+						   { name: 'Original watercolour', description: 'wishy washy but evocative', quantity: 1, price: 3500.00 }])
